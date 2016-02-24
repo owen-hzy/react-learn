433ad2895d9d65a9c646ae24d4532fceafe3fc1f
Owen HUANG <huangzhengyuan1993@gmail.com>
2016-02-24 22:40:33 +0800

    learned react state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyApalENR2Xka6cVygvULRFfKQv4Mi8cfCU"
 
 const App = function() {
-    return <div>Hi</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
